70c04981fffdae3497647c412eb0735895f8276a
onisim_albarov <onisim93@gmail.com>
2022-12-02 10:51:04 +0400

    complete stage1-module6-io-task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            StringBuilder profileData = new StringBuilder();
+            while (reader.ready()) {
+                profileData.append(reader.readLine()).append("\n");
+            }
+            String[] pairs = profileData.toString().split("\n");
+
+            for (String pair : pairs) {
+                String[] keyValue = pair.split(" ");
+                switch (keyValue[0]) {
+                    case "Name:": profile.setName(keyValue[1]);break;
+                    case "Age:": profile.setAge(Integer.parseInt(keyValue[1]));break;
+                    case "Email:": profile.setEmail(keyValue[1]);break;
+                    case "Phone:": profile.setPhone(Long.parseLong(keyValue[1]));break;
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
     }
+
 }
